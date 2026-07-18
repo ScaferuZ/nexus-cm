6d3c67f2476aa5bdddcef99961ded4bc698d7148
scaf <endrafruz@gmail.com>
2026-07-18 11:48:20 +0700

    fix: change some typos

--- tf/versions.tf
+++ tf/versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_versions = " >= 1.6.0"
+  required_version = " >= 1.6.0"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "~> 7.0"
     }
   }
